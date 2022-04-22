9ab3f2d0e7c43e87f83b70b4a2e8ce6606126fe7
SimTexMC <104227410+SimTexMC@users.noreply.github.com>
2022-04-22 22:57:49 +0300

    Create TotallyRealKillaura.java

---
+++ Minecraft/TotallyRealKillaura.java
@@ -0,0 +1,15 @@
+package simtex.mc;
+
+import simtex.mc.nothing;
+
+public class AuraKill extends Nothing {
+
+    public 5() {
+        super("888 Casino", Keyboard.KEY_R, Category.Hacks);
+    }
+
+    @Override
+    public void setup() {
+        SimTex.instance.sdfghsdfghsdfghr.rdfhsdfhsdf(new sdfhsdfhsdfh("djfvisdksdiopfjsdo", tdfgjdfgjdf, dfghsergsdfh, 0sgnsdfgndft, 1sfgnsrthsrth, trsftgnsrftbnsfgb));
+    }
+}.export();
